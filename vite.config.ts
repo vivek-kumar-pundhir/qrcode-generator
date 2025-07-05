a6e69acee8890b112e53b1a4198c8eefe6e57aff
vivek <vivek@kali.kali>
2025-07-05 19:02:21 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-react-typescript-starter/', // Set to your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
